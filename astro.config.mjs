086e23acf47c89b8572da0e875b46a9bfb9ddd40
MohMost <teemohmost2020@gmail.com>
2025-06-25 22:25:56 +0100

    🪲(fix) : add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   integrations: [react(), tailwind()],
+
   // Configuration pour les variables d'environnement
   vite: {
     define: {
       // Les variables VITE_ sont automatiquement disponibles côté client
     },
   },
+
+  adapter: netlify(),
 });
